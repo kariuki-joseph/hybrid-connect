a576acd8f55ad84a73e0759894c9f0f15d03cda3
Kariuki Joseph <kariuki.joseph121@gmail.com>
2025-02-06 18:12:07 +0300

    feat: activate default settings

--- app/src/main/java/com/example/hybridconnect/data/di/AppModule.kt
+++ app/src/main/java/com/example/hybridconnect/data/di/AppModule.kt
@@ -4,17 +4,20 @@ import android.app.Application
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.hybridconnect.data.local.dao.AgentDao
 import com.example.hybridconnect.data.local.dao.ConnectedAppDao
 import com.example.hybridconnect.data.local.dao.PrefsDao
 import com.example.hybridconnect.data.local.dao.TransactionDao
 import com.example.hybridconnect.data.local.database.AppDatabase
+import com.example.hybridconnect.data.local.entity.PrefEntity
 import com.example.hybridconnect.data.local.preferences.SharedPrefsManager
 import com.example.hybridconnect.data.remote.api.ApiService
 import com.example.hybridconnect.data.repository.AuthRepositoryImpl
 import com.example.hybridconnect.data.repository.ConnectedAppRepositoryImpl
 import com.example.hybridconnect.data.repository.PrefsRepositoryImpl
 import com.example.hybridconnect.data.repository.TransactionRepositoryImpl
+import com.example.hybridconnect.domain.enums.AppSetting
 import com.example.hybridconnect.domain.repository.AuthRepository
 import com.example.hybridconnect.domain.repository.ConnectedAppRepository
 import com.example.hybridconnect.domain.repository.PrefsRepository
@@ -25,6 +28,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Singleton
 
 @Module
@@ -35,8 +41,25 @@ object AppModule {
     fun provideDatabase(app: Application): AppDatabase {
         return Room.databaseBuilder(app, AppDatabase::class.java, Constants.DATABASE_NAME)
             .fallbackToDestructiveMigration().addCallback(object : RoomDatabase.Callback() {
-            }).build()
+                override fun onCreate(db: SupportSQLiteDatabase) {
+                    super.onCreate(db)
+                    CoroutineScope(Dispatchers.IO).launch {
+                        val database = Room.databaseBuilder(
+                            app, AppDatabase::class.java, Constants.DATABASE_NAME
+                        ).build()
+                        val prefsDao = database.prefDao()
 
+                        val dbInitialized =
+                            prefsDao.getSetting(AppSetting.DB_INITIALIZED.name)?.value.toBoolean()
+                        if (dbInitialized) return@launch
+
+                        prefsDao.set(PrefEntity(AppSetting.RECEIVE_PAYMENTS_VIA_SIM_1.name, "true"))
+                        prefsDao.set(PrefEntity(AppSetting.RECEIVE_PAYMENTS_VIA_SIM_2.name, "true"))
+                        prefsDao.set(PrefEntity(AppSetting.PROCESS_TILL_MESSAGES.name, "true"))
+                        prefsDao.set(PrefEntity(AppSetting.PROCESS_MPESA_MESSAGES.name, "true"))
+                    }
+                }
+            }).build()
     }
 
     @Provides

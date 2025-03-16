3943e65251c6d6e4da27f51c51a0b80891958634
Kariuki Joseph <kariuki.joseph121@gmail.com>
2025-03-16 12:45:36 +0300

    chore: increment app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.hybridconnect"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

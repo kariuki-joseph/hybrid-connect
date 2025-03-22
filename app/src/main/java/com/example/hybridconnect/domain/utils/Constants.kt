23300b8d17fc03015f4a2288373cb3ff008f57b9
Kariuki Joseph <kariuki.joseph121@gmail.com>
2025-03-22 14:00:42 +0300

    chore: switch to production mode

--- app/src/main/java/com/example/hybridconnect/domain/utils/Constants.kt
+++ app/src/main/java/com/example/hybridconnect/domain/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.hybridconnect.domain.utils
 object Constants {
     const val APP_NAME = "Hybrid Connect"
     const val DATABASE_NAME = "app_database"
-    const val BASE_URL = "https://test.api.bingwahybrid.com"
+    const val BASE_URL = "https://api.bingwahybrid.com"
     const val KEY_PREFS_NAME = "bingwa_prefs"
     const val KEY_ACCESS_TOKEN = "access_token"
     const val AIRTIME_SUBSCRIPTION_USSD_CODE = "*140*AMT*BH#"
